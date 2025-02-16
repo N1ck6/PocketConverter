44423408424ff6234da2fdb0f7a51f9efdfaa499
Nicki <91270829+N1ck6@users.noreply.github.com>
2025-02-16 20:55:57 +0300

    Update converter.py

--- converter.py
+++ converter.py
@@ -6,8 +6,6 @@ from io import StringIO
 from moviepy import VideoFileClip
 from win11toast import toast
 import traceback
-import winreg
-import ctypes
 
 all_list = ['mp4', 'gif', 'txt', 'pdf', 'docx', 'jpg', 'jpeg', 'png', 'webp', 'bmp', 'tiff', 'ico']
 log_file_path = "C:\\Program Files\\PocketConverter\\PocketConverter_log.txt"
@@ -17,80 +15,6 @@ else:
     base_path = os.path.dirname(__file__)
 icon_dir = os.path.join(base_path, 'logo.ico')
 font_dir = os.path.join(base_path, 'DejaVuSansCondensed.ttf')
-
-
-def run_as_admin():
-    if not ctypes.windll.shell32.IsUserAnAdmin():
-        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
-    return 0
-
-
-def check_registry_key():
-    registry_path = r"Software\Classes\SystemFileAssociations\.ico\shell\PocketConverter"
-    try:
-        with winreg.OpenKey(winreg.HKEY_CURRENT_USER, registry_path):
-            return True
-    except FileNotFoundError:
-        return False
-
-
-def get_extensions(extension):
-    # Get convert variants for each extension
-    ext = [['mp4', 'gif'], ['txt', 'pdf', 'docx'], ['jpg', 'jpeg', 'png', 'bmp', 'tiff', 'webp', 'ico'], ['jpg', 'png', 'webp', 'ico']]
-    for exts in ext:
-        if extension in exts:
-            if len(exts) == len(ext[2]):
-                ext = ext[3]
-                if extension in ext:
-                    ext.remove(extension)
-                return ext
-            else:
-                exts.remove(extension)
-                if extension == 'gif':
-                    exts.append('pngs')
-                    exts.append('png')
-                if extension == 'mp4':
-                    exts.append('mp3')
-                return exts
-
-
-def add_context_menu():  # Create context menu
-    try:
-        for extension in [i for i in all_list]:
-            # Creating Folder for each extension
-            key_path = fr"Software\Classes\SystemFileAssociations\.{extension}\shell\PocketConverter"
-            with winreg.CreateKey(winreg.HKEY_CURRENT_USER, key_path) as key:
-                winreg.SetValueEx(key, "MUIVerb", 0, winreg.REG_SZ, "Convert to")
-                winreg.SetValueEx(key, "Icon", 0, winreg.REG_SZ, r"C:\Program Files\PocketConverter\small.ico")
-                winreg.SetValueEx(key, "SubCommands", 0, winreg.REG_SZ, "")
-                winreg.SetValueEx(key, "MultiSelectModel", 0, winreg.REG_SZ, "Single")
-            c = 0
-            for sub_extension in get_extensions(extension):
-                # Dropdown buttons
-                subkey_path = key_path + fr"\shell\sub_one_{c}"
-                with winreg.CreateKey(winreg.HKEY_CURRENT_USER, subkey_path) as key:
-                    winreg.SetValueEx(key, "MUIVerb", 0, winreg.REG_SZ, f"{sub_extension}")
-                command_path = subkey_path + r"\command"
-                with winreg.CreateKey(winreg.HKEY_CURRENT_USER, command_path) as key:
-                    winreg.SetValueEx(key, "", 0, winreg.REG_SZ, f"C:\\Program Files\\PocketConverter\\converter.exe \"%1\" {sub_extension}")
-                c += 1
-        # Pics to pdf
-        key_path = r"Directory\shell\PocketConverter"
-        with winreg.CreateKey(winreg.HKEY_CLASSES_ROOT, key_path) as key:
-            winreg.SetValueEx(key, "MUIVerb", 0, winreg.REG_SZ, "Convert to")
-            winreg.SetValueEx(key, "Icon", 0, winreg.REG_SZ, r"C:\Program Files\PocketConverter\small.ico")
-            winreg.SetValueEx(key, "SubCommands", 0, winreg.REG_SZ, "")
-            winreg.SetValueEx(key, "MultiSelectModel", 0, winreg.REG_SZ, "Single")
-        folder_ones = ['pdf', 'gif']
-        for i in range(2):
-            subkey_path = key_path + fr"\shell\sub_one_{i}"
-            with winreg.CreateKey(winreg.HKEY_CLASSES_ROOT, subkey_path) as key:
-                winreg.SetValueEx(key, "MUIVerb", 0, winreg.REG_SZ, folder_ones[i])
-            command_path = subkey_path + r"\command"
-            with winreg.CreateKey(winreg.HKEY_CLASSES_ROOT, command_path) as key:
-                winreg.SetValueEx(key, "", 0, winreg.REG_SZ, f"C:\\Program Files\\PocketConverter\\converter.exe \"%V\" folder{folder_ones[i]}")
-    except Exception as e:
-        toast("Error:", e, icon=icon_dir)
 
 
 def create_name(filepath, mode):
@@ -320,12 +244,6 @@ def gif_to_photos(filepath, new_name):
 
 
 if __name__ == "__main__":
-    if not check_registry_key():
-        a = run_as_admin()
-        if ctypes.windll.shell32.IsUserAnAdmin():
-            add_context_menu()
-            toast("Context menu was added", "Enjoy optimised working with files", icon=icon_dir, group='done')
-        sys.exit()
     try:
         devnull = open(os.devnull, 'w')
         sys.stdout, sys.stderr = devnull, devnull
@@ -341,6 +259,8 @@ if __name__ == "__main__":
                 toast(f"Done!", f"Saved as extracted gif folder", icon=icon_dir, group='done')
             else:
                 toast(f"Done!", f"Saved as {name}.{mode}", icon=icon_dir, group='done')
+        else:
+            toast(f"Hi!", f"I need to be used through context menu ;)", icon=icon_dir, group='done')
     except Exception as e:
         if os.path.isfile(log_file_path) and logs_input and file and mode:
             with open(log_file_path, 'a') as log_file:  # Logs file to check what might have caused errors
@@ -348,6 +268,4 @@ if __name__ == "__main__":
                 log_file.write("Error Output:\n")
                 log_file.write(traceback.format_exc())
                 log_file.write('---------------------------------------------------------------------\n')
-        toast("Conversion error", f"Check log file in exe directory in Program Files for more information", icon=icon_dir)
-else:
-    toast("Not main launch", "App is not launched right", icon=icon_dir)
+        toast("Conversion error", fr"Check log file in in C:\Program Files\PocketConverter for more information", icon=icon_dir)
